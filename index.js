116b6e03aa7fc1bcf6b58c990e834d04d5f75df0
Michael Schwartz <mykesx@gmail.com>
2014-09-01 12:54:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mschwartz
+ * Date: 7/24/13
+ * Time: 7:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/*global decaf, require */
+
+"use strict";
+
+decaf.extend(exports, {
+    Memcached: require('lib/Memcached').Memcached
+});
